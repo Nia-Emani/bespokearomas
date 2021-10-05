b5aca417a3a5388f8e95bf62aa95dfd3f91a5b8e
niaemani <niaemanidickson@gmail.com>
2021-10-05 14:04:30 -0400

    setup resource and migrated tables, updated EDR in README

---
+++ db/migrate/20211005174634_create_fragrances.rb
@@ -0,0 +1,10 @@
+class CreateFragrances < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fragrances do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
