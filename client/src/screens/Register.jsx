9db7970951ad0fc33ac5834cfaf16a7a048ab339
niaemani <niaemanidickson@gmail.com>
2021-10-09 17:44:23 -0400

    added class names to register screen elements

--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -18,58 +18,54 @@ export default function Register(props) {
   };
 
   return (
-    <div>
+    <div className="register-card">
       <img
         className="register-image"
         src="https://lh3.googleusercontent.com/GiRRW2eS6qaBuPf9TwFIlXNbp0g8RXrileQ5uImpMrql2chO49g5NKJS4xVhqZxg3QCGtHLpwBzLVyYyDu_cItkghY4CoLALBgNHNHu6SHFvqDDNqluTEDXe5lpScqNzOTGkSkyxa_Y=h800"
       />
-      <form
-        className="register-form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          props.handleRegister(formData);
-        }}
-      >
+      <div className="register-form-container">
         <h3 className="register-greeting">Sign up to start rating!</h3>
-        <label>
-          Username
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        {/* <label>
-          Confirm Password
-          <input
-            type="password"
-            name="password-confirm"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label> */}
-        <Link to="/login">Login</Link>
-        <button className="register-button">Get Started</button>
-      </form>
+        <form
+          className="register-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            props.handleRegister(formData);
+          }}
+        >
+          <label className="register-label">
+            Username
+            <input
+              className="register-input"
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="register-label">
+            Email
+            <input
+              className="register-input"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label className="register-label">
+            Password
+            <input
+              className="register-input"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <Link to="/login">Login</Link>
+          <button className="register-button">Get Started</button>
+        </form>
+      </div>
     </div>
   );
 }
