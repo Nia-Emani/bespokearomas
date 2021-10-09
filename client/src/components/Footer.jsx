e2257ac958e5f5cd9e211c48bc8cf9581b081815
niaemani <niaemanidickson@gmail.com>
2021-10-09 17:30:48 -0400

    home screen styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
+import "../css/Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <ul className="footer-links-left">
-        <p>Partners</p>
-        <p>Parings</p>
-      </ul>
+    <div className="footer">
+      <p className="footer-link">Partners</p>
+      <p className="footer-link">Parings</p>
       <img
         className="icon"
         src="https://lh3.googleusercontent.com/XwafxzTikbjPRmNI6n7h8swBBXcq4v3DDUpnmWgKfM1HVEv2tesgV6cUw3oLMFGoH9uTELF_9d1jeZWAkBFci81XKM2TUDQlHuLvFJCc4A_zlRdtfszWt9Wi609EsgW6Aq-532R4CDg=h100"
       />
-      <ul className="footer-links-right">
-        <p>Press</p>
-        <p>Philanthropy</p>
-      </ul>
+      <p className="footer-link">Press</p>
+      <p className="footer-link">Philanthropy</p>
     </div>
   );
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -19,27 +19,40 @@ export default function Home() {
       </div>
       <div className="highest-rated-section">
         <h3 className="highest-rated-text">
-          This month's highest-rated fragrances
+          <i>This month's highest-rated fragrances</i>
         </h3>
-        <img
-          className="baccarat"
-          src="https://lh3.googleusercontent.com/CAcPm_6gcmnyaUMcy1zq6xWMNPgwIhxNqtebJ1tPzFnQ2f-XHp-64OnhbrXqP746MOtv6T1bZ5Ko95LfOoZcb6inCCaAMLLclbckbRnqcVXIENHrCH9_EG5N4_dGNOuzZ9OJNAN8PiA=h200"
-        />
-        <p>&#x2B50a;</p>
-        <img
-          className="byredo"
-          src="https://lh3.googleusercontent.com/OPTjZw-hxpr_23FBTbQWbAyQKAwn6ZcEw643c_reyVU2KWxMiHDfVtQYLCaBwKUBOEs3Ofyb8K9qeCV3xFtws_nNQkTCn9WCRwOAq7gKRifCMgypPv-1GjivYUdl65Kp-buYYPB2qxw=h200"
-        />
-        <img
-          className="clive"
-          src="https://lh3.googleusercontent.com/lVlREvazZecwC_SeAikiktn2oDN3JU-aco4kHS9gX81c4xJDnXOAXX9O0hVcZfp5kodjBGfsnhpfxuedJ3YJGUXWy8QwHDPYCJSMZ69bfKB6tWUJcrXApuo7QXfzHb3qEiOigk1C3VU=h200"
-        />
+        <div className="home-card-section">
+          <div className="home-cards">
+            <img
+              className="home-card-img"
+              src="https://lh3.googleusercontent.com/CAcPm_6gcmnyaUMcy1zq6xWMNPgwIhxNqtebJ1tPzFnQ2f-XHp-64OnhbrXqP746MOtv6T1bZ5Ko95LfOoZcb6inCCaAMLLclbckbRnqcVXIENHrCH9_EG5N4_dGNOuzZ9OJNAN8PiA=h200"
+            />
+            <p className="home-card-name">Baccarat Rouge 540</p>
+            <p className="home-rating">⭐ ⭐ ⭐ ⭐ ⭐</p>
+          </div>
+          <div className="home-cards">
+            <img
+              className="home-card-img"
+              src="https://lh3.googleusercontent.com/OPTjZw-hxpr_23FBTbQWbAyQKAwn6ZcEw643c_reyVU2KWxMiHDfVtQYLCaBwKUBOEs3Ofyb8K9qeCV3xFtws_nNQkTCn9WCRwOAq7gKRifCMgypPv-1GjivYUdl65Kp-buYYPB2qxw=h200"
+            />
+            <p className="home-card-name">Bal D'Afrique</p>
+            <p className="home-rating">⭐ ⭐ ⭐ ⭐ ⭐</p>
+          </div>
+          <div className="home-cards">
+            <img
+              className="home-card-img"
+              src="https://lh3.googleusercontent.com/lVlREvazZecwC_SeAikiktn2oDN3JU-aco4kHS9gX81c4xJDnXOAXX9O0hVcZfp5kodjBGfsnhpfxuedJ3YJGUXWy8QwHDPYCJSMZ69bfKB6tWUJcrXApuo7QXfzHb3qEiOigk1C3VU=h200"
+            />
+            <p className="home-card-name">No1 Feminine</p>
+            <p className="home-rating">⭐ ⭐ ⭐ ⭐ ⭐</p>
+          </div>
+        </div>
       </div>
       <div className="call-to-action-section">
         <h2 className="call-to-action">
           Help shoppers like you by rating your favorite luxury fragrances!
         </h2>
-        <button>
+        <button className="call-to-action-button">
           <Link to="/register">Get Started</Link>
         </button>
       </div>
