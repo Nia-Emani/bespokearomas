945022b8b01e68fbb3621077afc73b3a94eba36d
niaemani <niaemanidickson@gmail.com>
2021-10-09 21:21:22 -0400

    initial styling for login screen

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../css/Login.css";
 
 export default function Login(props) {
   const [formData, setFormData] = useState({
@@ -18,32 +19,32 @@ export default function Login(props) {
   return (
     <div className="login">
       <form
+        className="login-form"
         onSubmit={(e) => {
           e.preventDefault();
           props.handleLogin(formData);
         }}
       >
         <h3 className="login-greeting">Welcome back!</h3>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <Link to="/register">Register</Link>
-        <button>Submit</button>
+        <label>Username:</label>
+        <input
+          className="login-input"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+
+        <label>Password:</label>
+        <input
+          className="login-input"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button className="login-button">Submit</button>
       </form>
     </div>
   );
