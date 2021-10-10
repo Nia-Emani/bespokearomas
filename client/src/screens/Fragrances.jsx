1f632c29734484f48ca66595dfe7ff9da2b53a32
niaemani <niaemanidickson@gmail.com>
2021-10-09 23:04:05 -0400

    initial class names

--- client/src/screens/Fragrances.jsx
+++ client/src/screens/Fragrances.jsx
@@ -4,7 +4,7 @@ import "../css/Fragrances.css";
 
 export default function Fragrances(props) {
   return (
-    <div>
+    <div className="fragrances">
       <h3 className="fragrances-greeting1">
         Some of the world's most luxurious fragrances...
       </h3>
@@ -14,30 +14,34 @@ export default function Fragrances(props) {
         src="https://lh3.googleusercontent.com/B0wStKXtE_us57PsOUAUKbCj80GVZvTAYGGiKUmS77_meksGlVkojxTIoXIgniuK2kk9v1kE7nB_Ym8ao7N76EN08IAodQGUrOWXxP_o2lieoasnCCKnA7dopuAQGfedLFE9YRxFDWk=h80"
         alt="underline for emphasis on luxurious"
       />
-      {props.fragrances.map((fragrance) => (
-        <div>
-          <img
-            key={fragrance.image_url}
-            src={fragrance.image_url}
-            alt="fragrances bottle"
-          />
-          <p key={fragrance.id}>{fragrance.name} </p>
-          {fragrance.ratings.map((rating) => (
-            <StarRatings
-              rating={Number(rating.rank)}
-              starDimension="40px"
-              starSpacing="15px"
+      <div className="fragrances-container">
+        {props.fragrances.map((fragrance) => (
+          <div>
+            <img
+              className="fragrances-image"
+              key={fragrance.image_url}
+              src={fragrance.image_url}
+              alt="fragrances bottle"
             />
-          ))}
-          <button>
-            {/* <Link to={`/fragrances/${fragrance.id}/ratings/new`}> */}
-            <Link to={`/ratings/${fragrance.id}/new`}>Add Rating</Link>
-          </button>
-          <button>
-            <Link to={`/fragrance-detail/${fragrance.id}`}>Details</Link>
-          </button>
-        </div>
-      ))}
+            <p className="fragrances-name" key={fragrance.id}>
+              {fragrance.name}{" "}
+            </p>
+            {fragrance.ratings.map((rating) => (
+              <StarRatings
+                rating={Number(rating.rank)}
+                starDimension="40px"
+                starSpacing="15px"
+              />
+            ))}
+            <button className="add-button">
+              <Link to={`/ratings/${fragrance.id}/new`}>Add Rating</Link>
+            </button>
+            <button className="details-button">
+              <Link to={`/fragrance-detail/${fragrance.id}`}>Details</Link>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
